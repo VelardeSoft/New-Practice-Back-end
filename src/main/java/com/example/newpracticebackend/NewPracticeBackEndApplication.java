7288b8b42de29fdcdffd729b9723f7f2c3ce102c
Nestor Velarde <u20211c221@upc.edu.pe>
2024-06-14 15:35:04 -0500

    fix:initial commint

---
+++ src/main/java/com/example/newpracticebackend/NewPracticeBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.newpracticebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewPracticeBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewPracticeBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/newpracticebackend/NewPracticeBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.newpracticebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewPracticeBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
